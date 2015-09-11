3c2c469637f187cab45d8d8d3e0649bc4cb61cb0
Matt Bachmann <mbachmann@cloudlock.com>
2015-09-11 11:32:24 -0400

    Add the ability to wait

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+import time
+from flask import Flask, request
 app = Flask(__name__)
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return ''
+
+
 @app.route('/<code>', methods=['GET', 'POST'])
-def request(code):
-    return "The status of this request should be {}".format(code), int(code)
+def wait_request(code):
+    wait_time_param = request.args.get('wait', '0')
+    try:
+        wait_time = int(wait_time_param)
+    except ValueError:
+        return "Wait Time {} is invalid".format(wait_time_param)
+    time.sleep(wait_time)
+    return "The status of this request should be {}. I waitied {} seconds".format(code, wait_time), int(code)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
